575f6cc6b2f75c8938703e11eb326427958659c6
OleksandrNibyt <onibyt@mail.sfsu.edu>
2020-08-24 10:13:22 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,89 @@
+// setup
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const fake_database = [
+  {
+    id: 1,
+    name: 'Jeff',
+    age: 21
+  },
+  {
+    id: 2,
+    name: 'Alex',
+    age: 25
+  }
+];
+
+app.get("/", (request, response) => {
+  response.json(fake_database);
+});
+
+app.post('/user/new/', (request, response) => {
+  // get name and age from the req body
+  const { name, age } = request.body;
+  // generate an id
+  const id = fake_database.length + 1;
+  // create a new user object to store all the info
+  const newUser = {
+    id,
+    name,
+    age,
+  };
+  // add new user to the fake_database
+  fake_database.push(newUser);
+  // send success back in the response
+  response.json({ success: true });
+});
+
+
+app.get('/user/:id', (request, response) => {
+  // grab id parameter
+  const { id } = request.params;
+  // filter out the users that match the search id
+  const searchedUser = fake_database.filter(user => {
+    if (user.id === +id) return user;
+  });
+  // send searched user in the response
+  response.json(searchedUser);
+});
+
+
+app.put('/user/update/name/', (request, response) => {
+  // grab id and name from req body
+  const { name, id } = request.body;
+
+  // loop on fake_database
+  fake_database.forEach(user => {
+    // and if id matches edit name to new name
+    if (user.id === +id) {
+      user.name = name;
+    }
+
+  });
+  // send success back in the response
+  response.json({ success: true });
+});
+
+
+app.delete('/user/delete/:id', (request, response) => {
+// grab id parameter
+ const { id } = request.params;
+// using the id lookup the user with that id 
+ const index = fake_database.findIndex(user => user.id === +id);
+// and remove the user who matches
+ fake_database.splice(index, 1);
+// send success status with the response 
+ response.json({ success: true });
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
